fa5002df4a2995ea25331d8e486ccd26c31f4f99
makafis <vinnyalfieri@gmail.com>
2015-06-23 16:57:04 -0400

    complete

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,7 @@
 class Artist
   # code goes here
+  attr_accessor :name
+  def initialize(artist_name)
+    @name = artist_name
+  end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,16 @@
+require 'pry'
 class Song
   # code goes here
+  attr_accessor :title, :artist
+
+
+  def serialize
+    title = @title.gsub(" ","_").downcase
+    artist = @artist.name
+    filename = "#{title}"
+    # binding.pry
+    x = Tempfile.new([filename, '.txt'], 'tmp/')
+    x.write("#{artist} - #{@title}")
+    x.rewind
+  end
 end
